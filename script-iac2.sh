e33e1468dc6de36703310d0f12849c083f3d3475
marcelormaciel <marcelo.rezendemaciel@gmail.com>
2025-03-20 14:06:09 +0000

    Primeira versão do script

---
+++ script-iac2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#=====================================
+# Marcelo de Rezende Maciel
+# https://github.com/marcelormaciel
+# 20/03/2025 v0.1
+# script deve ser executado como root
+#=====================================
+
+echo "Atualizando servidor..."
+apt-get update
+apt-get upgrade -y
